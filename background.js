c9fc99dcba84d2a38b3613bcddb1f97155c0d696
Tingan Ho <tingan@clockies.com>
2012-05-20 22:45:27 +0200

    Change to the new slideshow ui

--- background.js
+++ background.js
@@ -36,7 +36,7 @@ var CURR_LIST,
 CURR_INDEX = 0,
 MAX_CARDS,
 currentTimer,
-board;
+slideshow;
 
 SC.renderCardNumberAndPointBadge = function() {
 	$('.list-card').each(function(){
@@ -91,40 +91,6 @@ SC.checkBoardURL = function() {
 	var regEx = /^https:\/\/trello.com\/board/;
 	return regEx.test(window.location.href);
 }
-SC.createTabs = function() {
-	if($('#board-header .sc-tab').length > 0){
-		return false;
-	}
-	var tabContainer = $('<div id=\'sc-tab-container\'></div>')[0];
-	$('#board-header .board-title').append(tabContainer);
-
-	var burnDownChart = $('<div class=\'sc-tab burn-down-chart intial\'><a>Burn-down chart</a></div>')[0];
-	$(tabContainer).append(burnDownChart);
-
-/*	var slideShow = $('<div class=\'sc-tab slide-show intial\'><a>Slideshow</a></div>')
-	$(tabContainer).append(slideShow);*/
-
-	var cards = $('<div class=\'sc-tab cards active intial\'><a>Cards</a></div>')
-	$(tabContainer).append(cards);
-
-	window.setTimeout(function(){ 
-		$('.sc-tab').addClass('final')
-		.removeClass('intial');
-		var showBurndDownChart = function(){
-			SC.showBurndDownChart(this);
-		}
-		$(burnDownChart).click(SC.showBurndDownChart);
-	}, 0);
-
-	
-
-
-}
-SC.showBurndDownChart = function(HTML_OBJ) {
-	/*$('#sc-perspective-wrapper #sc-wrapper').addClass('half-hidden').removeClass('intial');
-	$('#sc-chart').addClass('revealed').removeClass('hidden');*/
-	$(HTML_OBJ).unbind()
-}
 SC.wrapBody = function() {
 
 	if($('#sc-perspective-wrapper').length > 0){
@@ -136,59 +102,33 @@ SC.wrapBody = function() {
 	$(document.body).prepend(perspectiveWrapper);
 	$(wrapper).append($('#surface'));
 }
-SC.prependChart = function() {
+SC.prependSlideShow = function() {
 
-	if($('#sc-chart').length > 0){
+	if($('#sc-slide-show').length > 0){
 		return false;
 	}
-	var chart = $('<div id=\'sc-chart\' class=\'hidden\'></div>')[0];
-	$(document.body).prepend(chart);
+	slideshow = $('<div id=\'sc-slide-show\' class=\'hidden\'></div>')[0];
+	$(document.body).prepend(slideshow);
 }
-SC.renderTickets = function(listNumber) {
+SC.renderSlideShow = function(listNumber) {
 	if($('#sc-perspective-board').length > 0){
 		$('#sc-perspective-board').remove();
 	}
-
-	SC.prependChart();
-	var perspectiveBoard = $('<div id=\'sc-perspective-board\'></div>')[0];
-	$('#sc-chart').append(perspectiveBoard);
-	board = $('<ul id=\'sc-board\'></ul>')[0];
-	$(perspectiveBoard).append(board);
-	var col = 0,
-	n = 1,
-	top,
-	left;
-	$('.list:eq('+listNumber+') .list-card').each(function(){
-
-		var title = $(this).find('.list-card-title').text(),
-		removalOfPointsRegEx = /(#\d+)/g,
-		pointsRegEx = /#\d+/,
-		points = title.match(pointsRegEx),
-		title = points + ' ' + $.trim(title.replace(removalOfPointsRegEx, '')),
-		ticket = $('<li class=\'sc-ticket blurred\'>'+title+'</li>')[0];
-		$(board).append(ticket);
-		var rest = n%3, 
-		translate3d;
-		var x = col * 350;
-		switch(rest){
-			case 0:
-				left = x;
-				top = 660;
-				col++;
-				break;
-			case 1:
-				left = x;
-				top = 0;
-				break;
-			case 2:
-				left = x;
-				top = 350;
-				break;
-		}
-		$(ticket).css('top', top).css('left', left);
-		n++;
-	});
-	MAX_CARDS = n;
+	SC.prependSlideShow();
+	SC.renderLayout(listNumber);
+}
+SC.renderLayout = function(listNumber) {
+	var body = $('<div id=\'sc-body\'></div>')[0];
+	$(slideshow).append(body);
+	var leftBtn = $('<div id=\'sc-left-btn\'></div>')[0];
+	$(body).append(leftBtn);
+	var rightBtn = $('<div id=\'sc-right-btn\'></div>')[0];
+	$(body).append(rightBtn);
+	var ticketList = $('<div id=\'sc-ticket-list\'></div>')[0];
+	$(body).append(ticketList);
+	$('.list .list-card').each(function(){
+		$(this).attr('hej');
+	})	
 }
 SC.attachFullScreenRequest = function() {
 	var n = -1;
@@ -207,39 +147,6 @@ SC.attachFullScreenRequest = function() {
 		n++;
 	});
 }
-SC.focusTicket = function(ticketNumber) {
-	var ticket = $('.sc-ticket:eq('+ticketNumber+')')[0];
-	$('.sc-ticket').addClass('blurred')
-	.removeClass('focused');
-	$('.sc-ticket:eq('+ticketNumber+')').addClass('focused').removeClass('blurred')
-	var n = ticketNumber + 1;
-	var rest = n%3, 
-	translate3d;
-	var rest2 = ticketNumber % 3;
-	var col = (ticketNumber - rest2) / 3;
-	var x = col * 620;
-	SC.focusCamera(ticket)
-}
-SC.focusCamera = function(ticket) {
-	var top = (parseInt($(ticket).css('top').replace('px', ''), 10) - 200) + 'px';
-	var left = $(ticket).css('left');
-	$(board).css('margin-left','-'+left).css('margin-top', '-'+top).css('-webkit-transform-origin', left + ' 0px');
-}
-SC.tipBoard = function(leftOrRight) {
-	if(leftOrRight == 'right'){
-		board.style.webkitTransform = 'rotateY('+50+'deg)';
-	}else{
-		board.style.webkitTransform = 'rotateY(-'+50+'deg)';
-	}
-	$('#sc-board')[0].style.webkitTransformDuration = '330ms';
-	if(currentTimer){
-		clearTimeout(currentTimer);
-	}
-	currentTimer = setTimeout(function(){
-		board.style.webkitTransform = 'rotateY(0deg)';
-		board.style.webkitTransformDuration = '5s';
-	}, 330);
-}
 if(SC.checkBoardURL) {
 	var t = window.setInterval(function(){
 		if($('.list').length > 0) {
@@ -253,9 +160,9 @@ if(SC.checkBoardURL) {
 }
 document.addEventListener("webkitfullscreenchange", function(e){
    	if($.isDocumentInFullScreenMode()){
-   		SC.renderTickets(CURR_LIST);
+   		SC.renderSlideShow(CURR_LIST);
    	}else{
-   		$('#sc-chart').remove();
+   		$('#sc-slide-show').remove();
    	}
 }, false);
 $(window).bind('keydown', function(){
